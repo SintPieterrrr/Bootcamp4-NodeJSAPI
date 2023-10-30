bb494a608edb724deaf05536d063f0a7edd37256
SintPieterrrr <69983164+SintPieterrrr@users.noreply.github.com>
2023-10-30 10:27:19 +0100

    Add request method case.

--- api/index.js
+++ api/index.js
@@ -1,13 +1,26 @@
 const http = require('http');
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Node.js!');
+    console.log(req.method, req.url)
+
+    switch (req.method) {
+        case "GET":
+            // Response from out server
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end('{melding:"GET request succesful"}');
+            break;
+        default:
+            res.statusCode = 405;
+            res.setHeader('Content-Type', 'application/json');
+            res.end('{melding:"' + req.method +' not allowed"}');
+    }
+    // console.log(req)
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
